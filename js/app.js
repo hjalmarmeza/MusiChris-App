9b7fa4a2001d97fd6fec33fc8ef1f2480a70377d
hjalmarmeza <hjalmar.meza@gmail.com>
2026-03-28 11:09:39 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@
 
 
 
+
+
+
+
 // ==========================================================
 
 
@@ -11,7 +15,23 @@
 
 
 
+
+
+
+
+
+
+
+
 // LÓGICA DE NEGOCIO Y ACCIONES DE LA APP
+
+
+
+
+
+
+
+
 
 
 
@@ -28,58 +48,12 @@ const _u_m = 'aGphbG1hcg=='; // hjalmar
 const _p_m = 'MjU4NjMy'; // 258632
 
 
+
+
 function checkMasterAuth(u, p) {
     return btoa(u) === _u_m && btoa(p) === _p_m;
 }
 // ==========================================================
-
-
-// LÓGICA DE NEGOCIO Y ACCIONES DE LA APP
-
-
-// --- AUTENTICACIÓN ---
-async function handleLoginAttempt() {
-    const email = document.getElementById('loginEmail').value.trim().toLowerCase();
-    const pass = document.getElementById('loginPass').value.trim();
-    
-    if (checkMasterAuth(email, pass)) {
-        doLogin({ 
-            name: atob('SGphbG1hcg=='), 
-            email: atob('YWRtaW5AbXVzaWNocmlzLmNvbQ=='), 
-            role: atob('YWRtaW4='), 
-            avatar: ADMIN_AVATAR 
-        });
-        return;
-    }
-
-    
-
-
-    if (!appConfig.data) await loadAppData();
-
-
-    const user = appConfig.data?.users?.find(u => u.email.toLowerCase() === email);
-    if (user && pass === (user.password || atob('MTIz'))) {
-        doLogin(user);
-    } else {
-        showToast("Credenciales incorrectas", 'error');
-    }
-}
-
-
-    if (!appConfig.data) await loadAppData();
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
